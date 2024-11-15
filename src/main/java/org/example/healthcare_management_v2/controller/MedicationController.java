5532f474156eef28a2dd869a29285d7c3314cb95
nh19941995 <96659389+nh19941995@users.noreply.github.com>
2024-11-15 17:18:22 +0700

    - sửa lại 3.3. lấy thông tin bác sỹ bằng username - thêm username vào sss

--- src/main/java/org/example/healthcare_management_v2/controller/MedicationController.java
+++ src/main/java/org/example/healthcare_management_v2/controller/MedicationController.java
@@ -2,15 +2,14 @@ package org.example.healthcare_management_v2.controller;
 
 import lombok.AllArgsConstructor;
 import org.example.healthcare_management_v2.dto.medicationDto.MedicationDto;
+import org.example.healthcare_management_v2.map.MedicationMapper;
+import org.example.healthcare_management_v2.repositories.MedicationRepo;
 import org.example.healthcare_management_v2.service.MedicationService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -18,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicationController {
 
     private final MedicationService medicationService;
+    private final MedicationRepo medicationRepo;
+    private final MedicationMapper medicationMapper;
 
     // lấy danh sách thuốc
     // url: localhost:8080/api/medications
@@ -30,6 +31,15 @@ public class MedicationController {
         return ResponseEntity.ok(medicationService.findAll(pageable));
     }
 
-    // tìm kiếm thuốc theo tên
+    // lấy tên thuốc theo id
+    // url: localhost:8080/api/medications/medication_id
+    @GetMapping("/{medication_id}")
+    public ResponseEntity<MedicationDto> getMedicationById(
+            @PathVariable Long medication_id
+    ) {
+        MedicationDto medicationDto = medicationRepo.findById(medication_id).map(medicationMapper::medicationToMedicationDto)
+                .orElseThrow(() -> new RuntimeException("Medication not found"));
+        return ResponseEntity.ok(medicationDto);
+    }
 
 }
